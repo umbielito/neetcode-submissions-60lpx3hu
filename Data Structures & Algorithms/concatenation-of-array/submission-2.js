c7bb346f6e371d8a50bc392caef8221eaf8726cd
Gabriel Issao <gm.arr.gabriel@gmail.com>
2026-04-09 21:28:47 -0300

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        let ans = []
+        for (let i = 0; i < nums.length; i++) {
+            ans[i] = nums[i]
+            ans[i + nums.length] = nums[i]
+        }
+        return ans;
+    }
+}
